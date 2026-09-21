f85c2f5e46e058d84930c8f4f11e347fd15cbe7a
C-axyzpo <charlesaeronbabas@gmail.com>
2026-09-21 22:42:27 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("LIKAS Dashboard loaded successfully.");
+
+document.getElementById("activeEmergencies").textContent = "0";
+document.getElementById("onlineUnits").textContent = "0";
+document.getElementById("userReports").textContent = "0";
+document.getElementById("externalAlerts").textContent = "0";
